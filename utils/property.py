ea967c43596109eac016268103ca4a83fd85fb6d
Francky_ <frederick_spore@hotmail.com>
2022-02-28 16:59:58 +0100

    printing objects to csv test

--- utils/property.py
+++ utils/property.py
@@ -1,5 +1,4 @@
-from tkinter.messagebox import NO
-
+import csv
 
 class Property:
     """This class will represent a property"""
@@ -8,26 +7,21 @@ class Property:
         self.locality: str = None
         self.type: str = None
         self.sub_type: str = None
-
         self.price: float = None
         self.type_of_sale: str = None
-
         self.amount_of_rooms: int = None
         self.area: int = None
         self.has_full_kitchen: bool = None
         self.is_furnished: bool = None
         self.has_open_fire: bool = None
         self.has_pool: bool = None
-
         self.has_terrace: bool = None
         self.terrace_area: int = None
         self.has_garden: bool = None
         self.garden_area: int = None
-
         self.surface_land: int = None
         self.surface_area_plot: int = None
         self.amount_of_facades: int = None
-
         self.building_state: str = None
 
     def __init__(
@@ -97,3 +91,54 @@ class Property:
                 self.building_state,
             ]
         )
+
+    header = ["locality",
+               "type",
+               "sub_type",
+               "price",
+               "type_of_sale",
+               "amount_of_rooms",
+               "area",
+               "has_full_kitchen",
+               "is_furnished",
+               "has_open_fire",
+               "has_terrace",
+               "terrace_area",
+               "has_garden",
+               "garden_area",
+               "surface_land",
+               "surface_area_plot",
+               "amount_of_facades",
+               "has_pool",
+               "building_state"]
+
+
+test = Property(
+    "De Panne",
+    "House",
+    "rijhuis",
+    299000,
+    "normal",
+    4,
+    284,
+    True,
+    True,
+    False,
+    False,
+    None,
+    False,
+    None,
+    193,
+    284,
+    2,
+    False,
+    "normal"
+)
+t = [test,test]
+
+with open("test.csv","w",newline="") as stream:
+    writer = csv.writer(stream)
+    writer.writerow(Property.header)
+    writer.writerows(t)
+
+print("test")
