a8479bdccce740065ea62cc3a5d28a4487899b68
Francky_ <frederick_spore@hotmail.com>
2022-03-02 16:04:09 +0100

    deleted item_feat_data.py

--- utils/item_feat_data.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.immoweb.be/fr/annonce/maison/a-vendre/merelbeke/9820/9768055?searchId=621de6c461980"
-r = requests.get(url)
-print(url, r.status_code)
-soup = BeautifulSoup(r.content, "lxml")
-print(soup)
